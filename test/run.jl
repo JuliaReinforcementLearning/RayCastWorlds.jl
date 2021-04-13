16825b45d7901a7b4aedbb0675f8d89a3fbf5f44
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-04-13 13:07:07 +0530

    add method draw_agent

--- test/run.jl
+++ test/run.jl
@@ -7,17 +7,17 @@ const T = Float32
 
 # tile map
 
-const height_tm_tu = 16
-const width_tm_tu = 8
+const height_tm_tu = 8
+const width_tm_tu = 16
 
 const tm = RC.generate_tile_map(height_tm_tu, width_tm_tu)
 
 # agent
 
-const radius_wu = convert(T, 0.025)
+const radius_wu = convert(T, 0.05)
 const speed_wu = convert(T, 0.01)
 
-const agent = RC.Agent(SA.SVector(convert(T, 0.5), convert(T, 0.5)),
+const agent = RC.Agent(SA.SVector(convert(T, 0.5), convert(T, 0.25)),
                  SA.SVector(convert(T, 1/sqrt(2)), convert(T, 1/sqrt(2))),
                  speed_wu,
                  radius_wu,
@@ -25,15 +25,15 @@ const agent = RC.Agent(SA.SVector(convert(T, 0.5), convert(T, 0.5)),
 
 # world
 
-const height_world_wu = convert(T, 2)
-const width_world_wu = convert(T, 1)
+const height_world_wu = convert(T, 1)
+const width_world_wu = convert(T, 2)
 
 const world = RC.World(tm, height_world_wu, width_world_wu, agent)
 
 # img
 
-const height_img_pu = 512
-const width_img_pu = 256
+const height_img_pu = 256
+const width_img_pu = 512
 
 const img = zeros(UInt32, height_img_pu, width_img_pu)
 const fb = zeros(UInt32, width_img_pu, height_img_pu)
@@ -50,7 +50,9 @@ const blue = MFB.mfb_rgb(0, 0, 255)
 # extras
 
 const pu_per_wu = height_img_pu / height_world_wu
+@show pu_per_wu
 const pu_per_tu = height_img_pu ÷ height_tm_tu
+@show pu_per_tu
 const tu_per_wu = height_tm_tu / height_world_wu
 
 # main
@@ -61,6 +63,7 @@ get_start_tu(i_pu::Integer) = (i_pu - 1) ÷ pu_per_tu + 1
 get_start_tu(x_wu::AbstractFloat) = floor(Int, x_wu * tu_per_wu)
 
 const radius_pu = get_start_pu(radius_wu)
+const d = 2 * radius_pu - 1
 
 function draw_tile_map()
     map(CartesianIndices((1:height_tm_tu, 1:width_tm_tu))) do pos
@@ -79,10 +82,32 @@ function draw_tile_map()
     return nothing
 end
 
+function draw_agent()
+    center_i = get_start_pu(height_world_wu - agent.position[2])
+    center_j = get_start_pu(agent.position[1])
+    start_i = center_i - radius_pu + 1
+    start_j = center_j - radius_pu + 1
+    stop_i = start_i + d - 1
+    stop_j = start_j + d - 1
+
+    map(CartesianIndices((start_i:stop_i, start_j:stop_j))) do pos
+        i, j = pos.I
+
+        if (i - center_i) ^ 2 + (j - center_j) ^ 2 <= radius_pu ^ 2
+            img[pos] = gray
+        end
+
+        return nothing
+    end
+
+    return nothing
+end
+
 function render()
     window = MFB.mfb_open("Test", width_img_pu, height_img_pu)
 
     draw_tile_map()
+    draw_agent()
 
     while MFB.mfb_wait_sync(window)
 
