adad9bfb57fe632c2b81ee32946ba2f8b06e3e4d
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-04-15 19:37:34 +0530

    remove GW.Agent from tile map

--- src/world.jl
+++ src/world.jl
@@ -5,21 +5,18 @@ mutable struct Agent{T}
 end
 
 struct World{T}
-    tile_map::GW.GridWorldBase{Tuple{GW.Agent, GW.Wall}}
+    tile_map::GW.GridWorldBase{Tuple{GW.Wall}}
     height::T
     width::T
     agent::Agent{T}
 end
 
 function generate_tile_map(height = 8, width = 8)
-    objects = (GW.AGENT, GW.WALL)
+    objects = (GW.WALL,)
     tile_map = GW.GridWorldBase(objects, height, width)
 
     room = GW.Room(CartesianIndex(1, 1), height, width)
     GW.place_room!(tile_map, room)
-
-    agent_pos = CartesianIndex(2, 2)
-    tile_map[GW.AGENT, agent_pos] = true
 
     return tile_map
 end
