20797e32b1fcac952f990b209e57481e56d83d2f
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-05-15 22:35:58 +0530

    add environment SingleRoom (#15)
    * add package ReinforcementLearningBase
    
    * add SingleRoom environment
    
    * remove environment from random_policy.jl

--- src/RayCaster.jl
+++ src/RayCaster.jl
@@ -3,11 +3,13 @@ module RayCaster
 import GridWorlds as GW
 import MiniFB as MFB
 import StaticArrays as SA
+import ReinforcementLearningBase as RLBase
 
 include("utils.jl")
 include("geometry.jl")
 include("units.jl")
 include("drawing.jl")
 include("world.jl")
+include("single_room.jl")
 
 end
