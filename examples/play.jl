d37a4d09d7324775e963ec9f5352384747697d24
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-05-16 10:32:19 +0530

    play SingleRoom and save animations (#16)
    * add Random package
    
    * use SingleRoom environment and simplify run.jl
    
    * add ability to save play animations
    
    * save animations in output directory with unique names
    
    * rename run.jl to play.jl
    
    * ignore examples/output directory

---
+++ examples/play.jl
@@ -0,0 +1,93 @@
+import GridWorlds as GW
+import MiniFB as MFB
+import RayCaster as RC
+import StaticArrays as SA
+import Random
+import ColorTypes as CT
+import ImageMagick
+import FileIO
+import Dates
+import ReinforcementLearningBase as RLBase
+
+const T = Float32
+const seed = 123
+const rng = Random.MersenneTwister(seed)
+
+const env = RC.SingleRoom(T = T, rng = rng)
+
+step_number = 0
+
+total_reward = zero(T)
+
+const image_sequence = Vector{Matrix{UInt32}}()
+
+function keyboard_callback(window, key, mod, isPressed)::Cvoid
+    if isPressed
+        println(key)
+
+        if key == MFB.KB_KEY_UP
+            action = GW.MOVE_FORWARD
+        elseif key == MFB.KB_KEY_DOWN
+            action = RC.MOVE_BACKWARD
+        elseif key == MFB.KB_KEY_LEFT
+            action = GW.TURN_LEFT
+        elseif key == MFB.KB_KEY_RIGHT
+            action = GW.TURN_RIGHT
+        elseif key == MFB.KB_KEY_ESCAPE
+            MFB.mfb_close(window)
+            return nothing
+        end
+
+        global step_number += 1
+        env(action)
+        reward = RLBase.reward(env)
+        global total_reward += reward
+        is_terminated = RLBase.is_terminated(env)
+        push!(image_sequence, RC.get_combined_view(env))
+
+        println("step_number = $step_number")
+        println("action = $action")
+        println("reward = $reward")
+        println("total_reward = $total_reward")
+        println("is_terminated = $is_terminated")
+        println("***********************************")
+    end
+
+    return nothing
+end
+
+function play(env::RC.SingleRoom)
+    cv = RC.get_combined_view(env)
+    push!(image_sequence, RC.get_combined_view(env))
+
+    height_cv_pu, width_cv_pu = size(cv)
+    fb_cv = zeros(UInt32, width_cv_pu, height_cv_pu)
+
+    window = MFB.mfb_open("Combined View", width_cv_pu, height_cv_pu)
+    MFB.mfb_set_keyboard_callback(window, keyboard_callback)
+
+    permutedims!(fb_cv, RC.get_combined_view(env), (2, 1))
+
+    while MFB.mfb_wait_sync(window)
+        permutedims!(fb_cv, RC.get_combined_view(env), (2, 1))
+
+        state = MFB.mfb_update(window, fb_cv)
+
+        if state != MFB.STATE_OK
+            break;
+        end
+    end
+
+    return nothing
+end
+
+play(env)
+
+const gif = zeros(UInt32, size(RC.get_combined_view(env))..., length(image_sequence))
+
+for (i, image) in enumerate(image_sequence)
+    gif[:, :, i] .= image
+end
+
+file_name = joinpath("output", Dates.format(Dates.now(), "yyyy_mm_dd_HH_MM_SS")) * ".gif"
+FileIO.save(file_name, reinterpret.(CT.RGB24, gif), fps = 24)
