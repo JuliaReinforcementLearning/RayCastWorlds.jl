997cdc06ecd22a72215e32471b9a79afc5e43e43
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-16 14:41:09 +0530

    cleanup world.jl

--- src/world.jl
+++ src/world.jl
@@ -1,13 +1,13 @@
-function is_agent_colliding(tm, center_wu, wu_per_tu, tile_half_side_wu, radius_wu, height_world_wu)
-    height_tm_tu = size(tm, 2)
+function is_agent_colliding(tile_map, position_wu, wu_per_tu, tile_half_side_wu, radius_wu, height_world_wu)
+    height_tile_map_tu = GW.get_height(tile_map)
     square = StdSquare(tile_half_side_wu)
     circle = StdCircle(radius_wu)
-    return any(pos -> (tm[GW.WALL, pos] || tm[GW.GOAL, pos]) && is_colliding(square, circle, center_wu .- get_tile_center_wu(pos.I, wu_per_tu, height_tm_tu, tile_half_side_wu)), get_agent_region_tu(center_wu, radius_wu, wu_per_tu, height_world_wu))
+    return any(pos -> (tile_map[GW.WALL, pos] || tile_map[GW.GOAL, pos]) && is_colliding(square, circle, position_wu .- get_tile_center_wu(pos.I, wu_per_tu, height_tile_map_tu, tile_half_side_wu)), get_agent_region_tu(position_wu, radius_wu, wu_per_tu, height_world_wu))
 end
 
-function is_agent_colliding(tm, center_wu, wu_per_tu, tile_half_side_wu, radius_wu, height_world_wu, object)
-    height_tm_tu = size(tm, 2)
+function is_agent_colliding(tile_map, position_wu, wu_per_tu, tile_half_side_wu, radius_wu, height_world_wu, object)
+    height_tile_map_tu = GW.get_height(tile_map)
     square = StdSquare(tile_half_side_wu)
     circle = StdCircle(radius_wu)
-    return any(pos -> tm[object, pos] && is_colliding(square, circle, center_wu .- get_tile_center_wu(pos.I, wu_per_tu, height_tm_tu, tile_half_side_wu)), get_agent_region_tu(center_wu, radius_wu, wu_per_tu, height_world_wu))
+    return any(pos -> tile_map[object, pos] && is_colliding(square, circle, position_wu .- get_tile_center_wu(pos.I, wu_per_tu, height_tile_map_tu, tile_half_side_wu)), get_agent_region_tu(position_wu, radius_wu, wu_per_tu, height_world_wu))
 end
