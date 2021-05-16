d662347714dca1721dd2d262e3f666d68b455560
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-16 14:03:49 +0530

    rename img to image

--- src/drawing.jl
+++ src/drawing.jl
@@ -11,15 +11,15 @@ const dark_blue = MFB.mfb_rgb(0, 0, 127)
 
 # simple drawing method
 
-function draw_rectangle!(img::AbstractMatrix, top_left_i::Integer, top_left_j::Integer, bottom_right_i::Integer, bottom_right_j::Integer, value)
-    img[top_left_i:bottom_right_i, top_left_j:bottom_right_j] .= value
+function draw_rectangle!(image::AbstractMatrix, top_left_i::Integer, top_left_j::Integer, bottom_right_i::Integer, bottom_right_j::Integer, value)
+    image[top_left_i:bottom_right_i, top_left_j:bottom_right_j] .= value
 end
 
-function draw_circle!(img::AbstractMatrix, center_i::Integer, center_j::Integer, radius::Integer, value)
+function draw_circle!(image::AbstractMatrix, center_i::Integer, center_j::Integer, radius::Integer, value)
     for j in center_j - radius : center_j + radius
         for i in center_i - radius : center_i + radius
             if (center_i - i) .^ 2 + (center_j - j) .^ 2 <= radius ^ 2
-                img[i, j] = value
+                image[i, j] = value
             end
         end
     end
@@ -28,7 +28,7 @@ function draw_circle!(img::AbstractMatrix, center_i::Integer, center_j::Integer,
 end
 
 # Ref: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-function draw_line!(img::AbstractMatrix, i0::Int, j0::Int, i1::Int, j1::Int, value)
+function draw_line!(image::AbstractMatrix, i0::Int, j0::Int, i1::Int, j1::Int, value)
     di = abs(i1 - i0)
     dj = -abs(j1 - j0)
     si = i0 < i1 ? 1 : -1
@@ -36,7 +36,7 @@ function draw_line!(img::AbstractMatrix, i0::Int, j0::Int, i1::Int, j1::Int, val
     err = di + dj
 
     while true
-        img[i0, j0] = value
+        image[i0, j0] = value
 
         if (i0 == i1 && j0 == j1)
             break
@@ -58,12 +58,12 @@ function draw_line!(img::AbstractMatrix, i0::Int, j0::Int, i1::Int, j1::Int, val
     return nothing
 end
 
-function draw_tile_map!(img::AbstractMatrix, tile_map)
+function draw_tile_map!(image::AbstractMatrix, tile_map)
 
     height_tm_tu = size(tile_map, 2)
     width_tm_tu = size(tile_map, 3)
 
-    pu_per_tu = size(img, 1) ÷ height_tm_tu
+    pu_per_tu = size(image, 1) ÷ height_tm_tu
 
     for j in 1:width_tm_tu
         for i in 1:height_tm_tu
@@ -74,7 +74,7 @@ function draw_tile_map!(img::AbstractMatrix, tile_map)
             bottom_right_i = get_tile_stop_pu(i, pu_per_tu)
             bottom_right_j = get_tile_stop_pu(j, pu_per_tu)
 
-            draw_rectangle!(img, top_left_i, top_left_j, bottom_right_i, bottom_right_j, color)
+            draw_rectangle!(image, top_left_i, top_left_j, bottom_right_i, bottom_right_j, color)
         end
     end
 
@@ -93,12 +93,12 @@ function get_tile_color(tile_map, i::Integer, j::Integer)
     return color
 end
 
-function draw_tile_map_boundaries!(img::AbstractMatrix, pu_per_tu, color)
-    height_tv_pu = size(img, 1)
-    width_tv_pu = size(img, 2)
+function draw_tile_map_boundaries!(image::AbstractMatrix, pu_per_tu, color)
+    height_tv_pu = size(image, 1)
+    width_tv_pu = size(image, 2)
 
-    img[1:pu_per_tu:height_tv_pu, :] .= color
-    img[:, 1:pu_per_tu:width_tv_pu] .= color
+    image[1:pu_per_tu:height_tv_pu, :] .= color
+    image[:, 1:pu_per_tu:width_tv_pu] .= color
 
     return nothing
 end
