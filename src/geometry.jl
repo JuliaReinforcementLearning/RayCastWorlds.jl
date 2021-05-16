901f6b81bfb7d0746724eed1d27b844ba983d2a8
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-16 14:47:17 +0530

    cleanup geometry.jl

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,8 +1,8 @@
 rotate(x, y, c, s) = (c * x - s * y, s * x + c * y)
-rotate(vec::SA.SVector, dir::SA.SVector) = SA.SVector(rotate(vec[1], vec[2], dir[1], dir[2]))
-rotate_plus_90(vec::SA.SVector) = SA.SVector(-vec[2], vec[1])
-rotate_minus_90(vec::SA.SVector) = SA.SVector(vec[2], -vec[1])
-rotate_180(vec::SA.SVector) = -vec
+rotate(position::SA.SVector, direction::SA.SVector) = SA.SVector(rotate(position[1], position[2], direction[1], direction[2]))
+rotate_plus_90(position::SA.SVector) = SA.SVector(-position[2], position[1])
+rotate_minus_90(position::SA.SVector) = SA.SVector(position[2], -position[1])
+rotate_180(position::SA.SVector) = -position
 
 struct StdSquare{T}
     half_side::T
@@ -16,19 +16,19 @@ end
 
 get_radius(circle::StdCircle) = circle.radius
 
-function get_projection(square::StdSquare, pos::SA.SVector)
+function get_projection(square::StdSquare, position::SA.SVector)
     half_side = get_half_side(square)
-    return clamp.(pos, -half_side, half_side)
+    return clamp.(position, -half_side, half_side)
 end
 
-function is_colliding(square::StdSquare, circle::StdCircle, pos::SA.SVector)
-    projection = get_projection(square, pos)
-    vec = pos - projection
+function is_colliding(square::StdSquare, circle::StdCircle, position::SA.SVector)
+    projection = get_projection(square, position)
+    vec = position - projection
     radius = get_radius(circle)
     return sum(vec .^ 2) < radius * radius
 end
 
-function get_rays(dir::SA.SVector, semi_fov, num_rays)
-    angle = atan(dir[2], dir[1])
+function get_rays(direction::SA.SVector, semi_fov, num_rays)
+    angle = atan(direction[2], direction[1])
     return map(theta -> SA.SVector(cos(theta), sin(theta)), range(angle - semi_fov, angle + semi_fov, length = num_rays))
 end
