a0c300c1df314b1e9d483ae1a004630c3de727bf
Siddharth Bhatia <sidb60200@gmail.com>
2021-06-18 18:25:54 +0530

    remove simple drawing methods

--- src/drawing.jl
+++ src/drawing.jl
@@ -12,57 +12,6 @@ const blue = MFB.mfb_rgb(0, 0, 255)
 const dark_blue = MFB.mfb_rgb(0, 0, 127)
 
 #####
-##### simple drawing methods
-#####
-
-function draw_rectangle!(image::AbstractMatrix, top_left_i::Integer, top_left_j::Integer, bottom_right_i::Integer, bottom_right_j::Integer, value)
-    image[top_left_i:bottom_right_i, top_left_j:bottom_right_j] .= value
-end
-
-function draw_circle!(image::AbstractMatrix, center_i::Integer, center_j::Integer, radius::Integer, value)
-    for j in center_j - radius : center_j + radius
-        for i in center_i - radius : center_i + radius
-            if (center_i - i) .^ 2 + (center_j - j) .^ 2 <= radius ^ 2
-                image[i, j] = value
-            end
-        end
-    end
-
-    return nothing
-end
-
-# Ref: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-function draw_line!(image::AbstractMatrix, i0::Int, j0::Int, i1::Int, j1::Int, value)
-    di = abs(i1 - i0)
-    dj = -abs(j1 - j0)
-    si = i0 < i1 ? 1 : -1
-    sj = j0 < j1 ? 1 : -1
-    err = di + dj
-
-    while true
-        image[i0, j0] = value
-
-        if (i0 == i1 && j0 == j1)
-            break
-        end
-
-        e2 = 2 * err
-
-        if (e2 >= dj)
-            err += dj
-            i0 += si
-        end
-
-        if (e2 <= di)
-            err += di
-            j0 += sj
-        end
-    end
-
-    return nothing
-end
-
-#####
 ##### draw tile map
 #####
 
