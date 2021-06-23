b75c63935db04e807a403d93aca0cffe6dcf8c44
Siddharth Bhatia <sidb60200@gmail.com>
2021-06-23 22:17:38 +0530

    cleanup units.jl

--- src/units.jl
+++ src/units.jl
@@ -1,27 +1,3 @@
-# conversion single coordinate
-
 wu_to_tu(x_wu::AbstractFloat) = floor(Int, x_wu) + 1
 wu_to_pu(x_wu::AbstractFloat, pu_per_wu) = floor(Int, x_wu * pu_per_wu) + 1
-wu_to_tu(x_wu::AbstractFloat, wu_per_tu) = floor(Int, x_wu / wu_per_tu) + 1
 pu_to_tu(i_pu::Integer, pu_per_tu) = (i_pu - 1) ÷ pu_per_tu + 1
-
-# others
-
-get_tile_start_pu(i_tu, pu_per_tu) = (i_tu - 1) * pu_per_tu + 1
-get_tile_stop_pu(i_tu, pu_per_tu) = i_tu * pu_per_tu
-
-# tile region
-
-get_tile_bottom_left_wu((i_tu, j_tu), wu_per_tu, height_tm_tu) = ((j_tu - 1) * wu_per_tu, (height_tm_tu - i_tu) * wu_per_tu)
-get_tile_center_wu(tile_tu, wu_per_tu, height_tm_tu, tile_half_side_wu) = get_tile_bottom_left_wu(tile_tu, wu_per_tu, height_tm_tu) .+ tile_half_side_wu
-
-# agent region
-
-get_agent_bottom_left_tu(center_wu, radius_wu, wu_per_tu, height_world_wu) = wu_to_tu(center_wu .- radius_wu, wu_per_tu, height_world_wu)
-get_agent_top_right_tu(center_wu, radius_wu, wu_per_tu, height_world_wu) = wu_to_tu(center_wu .+ radius_wu, wu_per_tu, height_world_wu)
-
-function get_agent_region_tu(center_wu, radius_wu, wu_per_tu, height_world_wu)
-    start_i, stop_j = get_agent_top_right_tu(center_wu, radius_wu, wu_per_tu, height_world_wu)
-    stop_i, start_j = get_agent_bottom_left_tu(center_wu, radius_wu, wu_per_tu, height_world_wu)
-    return CartesianIndices((start_i:stop_i, start_j:stop_j))
-end
