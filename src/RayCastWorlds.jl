7d5593f1b47947f50ce4c753d42ed1203b429959
Siddharth Bhatia <sidb60200@gmail.com>
2021-06-28 18:31:31 +0530

    remove GridWorlds, RLBase. add RayCaster. rename geometry.jl to collision_detection.jl

--- src/RayCastWorlds.jl
+++ src/RayCastWorlds.jl
@@ -1,15 +1,14 @@
 module RayCastWorlds
 
-import GridWorlds as GW
 import MiniFB as MFB
 import Random
+import RayCaster as RC
 import REPL
-import ReinforcementLearningBase as RLBase
 import SimpleDraw as SD
 import StaticArrays as SA
 
 include("play.jl")
-include("geometry.jl")
+include("collision_detection.jl")
 include("units.jl")
 include("drawing.jl")
 include("world.jl")
