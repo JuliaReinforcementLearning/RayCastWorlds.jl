7eb01c9e74f2c46596496c34a7b1f650509ffda4
Siddharth Bhatia <sidb60200@gmail.com>
2021-07-27 18:26:45 +0530

    remove types from method signatures in units.jl

--- src/units.jl
+++ src/units.jl
@@ -1,3 +1,3 @@
-wu_to_tu(x_wu::AbstractFloat) = floor(Int, x_wu) + 1
-wu_to_pu(x_wu::AbstractFloat, pu_per_wu) = floor(Int, x_wu * pu_per_wu) + 1
-pu_to_tu(i_pu::Integer, pu_per_tu) = (i_pu - 1) ÷ pu_per_tu + 1
+wu_to_tu(x_wu) = floor(Int, x_wu) + 1
+wu_to_pu(x_wu, pu_per_wu) = floor(Int, x_wu * pu_per_wu) + 1
+pu_to_tu(i_pu, pu_per_tu) = (i_pu - 1) ÷ pu_per_tu + 1
