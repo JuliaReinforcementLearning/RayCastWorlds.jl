a0dbfdd4296a11967d8fa844bbd49b16d59f973e
Siddharth Bhatia <sidb60200@gmail.com>
2021-08-10 17:51:20 +0530

    remove navigation.jl

--- src/navigation.jl
+++
@@ -1,5 +0,0 @@
-turn_left(direction_au, num_directions) = mod(direction_au + 1, num_directions)
-turn_right(direction_au, num_directions) = mod(direction_au - 1, num_directions)
-
-move_forward(position_wu, direction_wu, position_increment_wu) = position_wu + position_increment_wu * direction_wu
-move_backward(position_wu, direction_wu, position_increment_wu) = position_wu - position_increment_wu * direction_wu
