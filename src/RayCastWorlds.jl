d9a59b1b21dd1e0343a9623a48636b5c1114f0f9
Siddharth Bhatia <sidb60200@gmail.com>
2021-08-10 17:50:23 +0530

    clean up files

--- src/RayCastWorlds.jl
+++ src/RayCastWorlds.jl
@@ -2,68 +2,19 @@ module RayCastWorlds
 
 import ReinforcementLearningBase as RLBase
 
-reset!(world) = error("Method not implemented")
-act!(world) = error("Method not implemented")
-cast_rays!(world) = error("Method not implemented")
-get_action_keys(env) = error("Method not implemented")
-get_action_names(env) = error("Method not implemented")
-play!(env) = error("Method not implemented")
-update_top_view!(env) = error("Method not implemented")
-update_camera_view!(env) = error("Method not implemented")
-
-function copy_image_to_frame_buffer!(frame_buffer, image)
-    height_image, width_image = size(image)
-    for j in 1:width_image
-        for i in 1:height_image
-            frame_buffer[j, i] = image[i, j]
-        end
-    end
-
-    return nothing
-end
-
-function sample_empty_position(rng, tile_map, region, max_tries)
-    position = rand(rng, region)
-
-    for i in 1:max_tries
-        if any(@view tile_map[:, position])
-            position = rand(rng, region)
-        else
-            return position
-        end
-    end
-
-    @warn "Could not sample an empty position in max_tries = $(max_tries). Returning non-empty position: $(position)"
-
-    return position
-end
-
-function sample_empty_position(rng, tile_map, region)
-    max_tries = 1024 * length(region)
-    position = sample_empty_position(rng, tile_map, region, max_tries)
-    return position
-end
-
-function sample_empty_position(rng, tile_map, max_tries::Integer)
-    _, height, width = size(tile_map)
-    region = CartesianIndices((1 : height, 1 : width))
-    position = sample_empty_position(rng, tile_map, region, max_tries)
-    return position
-end
-
-function sample_empty_position(rng, tile_map)
-    _, height, width = size(tile_map)
-    region = CartesianIndices((1 : height, 1 : width))
-    max_tries = 1024 * height * width
-    position = sample_empty_position(rng, tile_map, region, max_tries)
-    return position
-end
-
 abstract type AbstractGame end
 
-include("units.jl")
+function reset! end
+function act! end
+function cast_rays! end
+function get_action_keys end
+function get_action_names end
+function play! end
+function update_top_view! end
+function update_camera_view! end
+
+include("utils.jl")
 include("collision_detection.jl")
-include("navigation.jl")
 include("rlbase.jl")
 include("single_room.jl")
 
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,73 @@
+#####
+##### unit conversions
+#####
+
 wu_to_tu(x_wu) = floor(Int, x_wu) + 1
 wu_to_pu(x_wu, pu_per_wu) = floor(Int, x_wu * pu_per_wu) + 1
 pu_to_tu(i_pu, pu_per_tu) = (i_pu - 1) ÷ pu_per_tu + 1
+
+#####
+##### navigation
+#####
+
+turn_left(direction_au, num_directions) = mod(direction_au + 1, num_directions)
+turn_right(direction_au, num_directions) = mod(direction_au - 1, num_directions)
+
+move_forward(position_wu, direction_wu, position_increment_wu) = position_wu + position_increment_wu * direction_wu
+move_backward(position_wu, direction_wu, position_increment_wu) = position_wu - position_increment_wu * direction_wu
+
+#####
+##### sampling empty tiles on the tile map
+#####
+
+function sample_empty_position(rng, tile_map, region, max_tries)
+    position = rand(rng, region)
+
+    for i in 1:max_tries
+        if any(@view tile_map[:, position])
+            position = rand(rng, region)
+        else
+            return position
+        end
+    end
+
+    @warn "Could not sample an empty position in max_tries = $(max_tries). Returning non-empty position: $(position)"
+
+    return position
+end
+
+function sample_empty_position(rng, tile_map, region)
+    max_tries = 1024 * length(region)
+    position = sample_empty_position(rng, tile_map, region, max_tries)
+    return position
+end
+
+function sample_empty_position(rng, tile_map, max_tries::Integer)
+    _, height, width = size(tile_map)
+    region = CartesianIndices((1 : height, 1 : width))
+    position = sample_empty_position(rng, tile_map, region, max_tries)
+    return position
+end
+
+function sample_empty_position(rng, tile_map)
+    _, height, width = size(tile_map)
+    region = CartesianIndices((1 : height, 1 : width))
+    max_tries = 1024 * height * width
+    position = sample_empty_position(rng, tile_map, region, max_tries)
+    return position
+end
+
+#####
+##### transpose and copy image to minifb frame buffer
+#####
+
+function copy_image_to_frame_buffer!(frame_buffer, image)
+    height_image, width_image = size(image)
+    for j in 1:width_image
+        for i in 1:height_image
+            frame_buffer[j, i] = image[i, j]
+        end
+    end
+
+    return nothing
+end
